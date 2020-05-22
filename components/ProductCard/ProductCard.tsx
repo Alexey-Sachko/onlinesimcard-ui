71eb18854909d34fd8ffddec14126a136f9f90e8
Alexey Sachko <alexey.sachko@gmail.com>
2020-05-22 14:58:29 +0500

    фикс зависимостей

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
-import { Button, Paper } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
   root: {
