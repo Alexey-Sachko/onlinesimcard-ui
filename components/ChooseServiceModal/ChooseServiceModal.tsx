a8b41057a7889138c78e895885146e904caa7987
Alexey Sachko <alexey.sachko@gmail.com>
2020-05-22 14:56:33 +0500

    Добавил сервисы

--- components/ChooseServiceModal/ChooseServiceModal.tsx
+++ components/ChooseServiceModal/ChooseServiceModal.tsx
@@ -1,43 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
+// import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogActions from "@material-ui/core/DialogActions";
 import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
-import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+
+import { services } from "../../data/services";
 
 type Props = {
   open: boolean;
   onClose?: () => void;
 };
 
-type Service = {
-  name: string;
-  x: number;
-  y: number;
-};
-
 const ICON_SIZE = 16;
-
-const services: Service[] = [
-  {
-    name: "Вкотакте",
-    x: 0,
-    y: 0,
-  },
-  {
-    name: "Facebook",
-    x: 1,
-    y: 0,
-  },
-  {
-    name: "Одноклассники",
-    x: 2,
-    y: 0,
-  },
-];
 
 const useStyles = makeStyles((theme) => ({
   socialIcon: {
@@ -45,19 +26,22 @@ const useStyles = makeStyles((theme) => ({
     width: "16px",
     height: "16px",
   },
+  socialIconWrapper: {
+    minWidth: "30px",
+  },
 }));
 
 const ChooseServiceModal = ({ open, onClose }: Props) => {
   const classes = useStyles();
   return (
-    <Dialog open={open} maxWidth="md" fullWidth onClose={onClose}>
+    <Dialog open={open} maxWidth="sm" fullWidth onClose={onClose}>
       <DialogTitle>Выберите сервис</DialogTitle>
       <DialogContent>
         {/* <DialogContentText>Some text</DialogContentText> */}
         <List>
           {services.map(({ name, x, y }, idx) => (
-            <ListItem key={idx} button onClick={onClose}>
-              <ListItemIcon>
+            <ListItem key={idx} button onClick={onClose} divider>
+              <ListItemIcon className={classes.socialIconWrapper}>
                 <div
                   className={classes.socialIcon}
                   style={{
---
+++ data/services.ts
@@ -0,0 +1,43 @@
+export type Service = {
+  name: string;
+  x: number;
+  y: number;
+};
+
+export const services: Service[] = [
+  {
+    name: "Вкотакте",
+    x: 15,
+    y: 2,
+  },
+  {
+    name: "Facebook",
+    x: 1,
+    y: 0,
+  },
+  {
+    name: "Одноклассники",
+    x: 2,
+    y: 0,
+  },
+  {
+    name: "Mail.ru",
+    x: 0,
+    y: 1,
+  },
+  {
+    name: "Gmail",
+    x: 2,
+    y: 1,
+  },
+  {
+    name: "Avito",
+    x: 2,
+    y: 2,
+  },
+  {
+    name: "Юла",
+    x: 1,
+    y: 5,
+  },
+];
