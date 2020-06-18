8b7c1614732cb24c05f42ad3259dc1ae4d19031a
Alexey Sachko <alexey.sachko@gmail.com>
2020-06-18 10:17:50 +0500

    Пофиксил редакс

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,65 +1,71 @@
-import NextApp from "next/app";
+import App, { AppInitialProps, AppContext } from "next/app";
 import Head from "next/head";
 import React from "react";
-import withRedux from "next-redux-wrapper";
-import { Provider } from "react-redux";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import { YMInitializer } from "react-yandex-metrika";
 import { THEME } from "../theme";
-import { makeStore, AppStore } from "../redux/index";
+import { wrapper } from "../redux/index";
 
-type AppPropsWithStore = {
-  store: AppStore;
-};
+class MyApp extends App<AppInitialProps> {
+  public static getInitialProps = async ({ Component, ctx }: AppContext) => {
+    ctx.store.dispatch({ type: "TOE", payload: "was set in _app" });
 
-class App extends NextApp<AppPropsWithStore> {
-  // remove it here
+    return {
+      pageProps: {
+        // Call page-level getInitialProps
+        ...(Component.getInitialProps
+          ? await Component.getInitialProps(ctx)
+          : {}),
+        // Some custom thing for all pages
+        pathname: ctx.pathname,
+      },
+    };
+  };
+
   componentDidMount() {
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles && jssStyles.parentNode)
       jssStyles.parentNode.removeChild(jssStyles);
   }
   render() {
-    const { Component, pageProps, store } = this.props;
+    const { Component, pageProps } = this.props;
     return (
-      <Provider store={store}>
-        <MuiThemeProvider theme={THEME}>
-          <Head>
-            {/* <meta name="theme-color" content="#292929" /> */}
-            <meta name="language" content="ru" />
-            {/* <meta name="keywords" content={keywords.join(", ")} /> */}
-            {/* <meta
+      <MuiThemeProvider theme={THEME}>
+        <Head>
+          {/* <meta name="theme-color" content="#292929" /> */}
+          <meta name="language" content="ru" />
+          {/* <meta name="keywords" content={keywords.join(", ")} /> */}
+          {/* <meta
             name="description"
             content="Descr"
           /> */}
-            <link
-              rel="shortcut icon"
-              type="image/x-icon"
-              href="/static/favicon.ico"
-            />
-            <title>Виртуальный номер для приема СМС</title>
-            {/* <link
+          <link
+            rel="shortcut icon"
+            type="image/x-icon"
+            href="/static/favicon.ico"
+          />
+          <title>Виртуальный номер для приема СМС</title>
+          {/* <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           /> */}
-            <link
-              href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@600&display=swap"
-              rel="stylesheet"
-            />
-            <link
-              href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap"
-              rel="stylesheet"
-            />
-          </Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@600&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
 
-          <YMInitializer accounts={[62981725]} />
-          <CssBaseline />
-          <Component {...pageProps} />
-        </MuiThemeProvider>
-      </Provider>
+        <YMInitializer accounts={[62981725]} />
+        <CssBaseline />
+        <Component {...pageProps} />
+      </MuiThemeProvider>
     );
   }
 }
 
-export default withRedux(makeStore)(App);
+export default wrapper.withRedux(MyApp);
--- redux/index.ts
+++ redux/index.ts
@@ -1,17 +1,18 @@
 import { configureStore, combineReducers, AnyAction } from "@reduxjs/toolkit";
 import { NextPageContext, NextComponentType } from "next";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
+import { MakeStore, createWrapper, Context } from "next-redux-wrapper";
 import * as reducers from "./reducers";
 
 const rootReducer = combineReducers(reducers);
 
-export const makeStore = (initialState: any) => {
+export const makeStore = (context) => {
   return configureStore({
-    preloadedState: initialState,
     reducer: rootReducer,
   });
 };
 export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const wrapper = createWrapper<RootState>(makeStore, { debug: false });
 
 export type AppStore = ReturnType<typeof makeStore>;
 export type AppDispatch = AppStore["dispatch"];
