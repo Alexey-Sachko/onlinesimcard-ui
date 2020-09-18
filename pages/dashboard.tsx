df9804faf03f625c3e350b2a31ae8eee050ad3fe
Alexey Sachko <alexey.sachko@gmail.com>
2020-09-18 23:13:42 +0500

    пофикшена сборка

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-import { PageWithStore } from "../redux";
-import { useTypedSelector } from "../redux/index";
 
-const Dashboard: PageWithStore = () => {
-  const { user } = useTypedSelector((state) => state);
-  // return <div>email: {user.email}</div>;
+const Dashboard = () => {
   return null;
 };
 
