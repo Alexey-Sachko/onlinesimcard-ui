f4b7c5d8c2ca81b5997deeaa086e5e3e1163b776
Alexey Sachko <alexey.sachko@gmail.com>
2020-10-06 21:09:32 +0500

    добавлены иконки сервисов

--- components/Dashboard/Services/icons/iconsMap.ts
+++ components/Dashboard/Services/icons/iconsMap.ts
@@ -1,7 +1,23 @@
 import vk from "./vk.png";
 import ot from "./ot.png";
+import ok from "./ok.png";
+import tg from "./tg.png";
+import qw from "./qw.png";
+import ub from "./ub.png";
+import fb from "./fb.png";
+import go from "./go.png";
+import av from "./av.png";
+import tw from "./tw.png";
 
 export const iconsMap = {
   vk,
   ot,
+  ok,
+  tg,
+  qw,
+  ub,
+  fb,
+  go,
+  av,
+  tw,
 };
