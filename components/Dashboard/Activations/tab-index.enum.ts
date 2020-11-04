0042956544881ce2fed0a46c6d7f9afb44f1fcc4
Alexey Sachko <alexey.sachko@gmail.com>
2020-11-04 15:22:54 +0500

    feat: payments, разделение на экраны в Dashboard, кнопка пополнить

---
+++ components/Dashboard/Activations/tab-index.enum.ts
@@ -0,0 +1,4 @@
+export enum TabIndex {
+  Services,
+  Activations,
+}
---
+++ components/Dashboard/Payment/Payment.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Payment = () => {
+  return <div>sdsdads</div>;
+};
+
+export default Payment;
