5fcfd7d0a754b088121ec1b60bd399db8d0a03e2
Aleksandr Snigirev <snigirev.a@avtocod.ru>
2020-12-17 21:25:36 +0500

    уменьшен интервал

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -38,8 +38,8 @@ export const useAuth = (): AuthData => {
     { data, loading, called, error, startPolling, stopPolling },
   ] = useMeLazyQuery({
     onError: () => stopPolling(),
-    onCompleted: () => startPolling(3500),
-    pollInterval: 3500,
+    onCompleted: () => startPolling(3000),
+    pollInterval: 3000,
     fetchPolicy: "cache-and-network",
   });
   const [logoutRequest] = useLogoutMutation();
